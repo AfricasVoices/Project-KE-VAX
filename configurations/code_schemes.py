51a9b1e39d1ae3364be8915a323476b3d519908a
Isaack Mwenda <mwendaisaac@gmail.com>
2021-06-23 11:57:31 +0300

    add engagement poll code scheme configurations

--- configurations/code_schemes.py
+++ configurations/code_schemes.py
@@ -13,6 +13,12 @@ class CodeSchemes(object):
     VACCINATION_THOUGHTS = _open_scheme("vaccination_thoughts.json")
     OTHER_MESSAGES = _open_scheme("other_messages.json")
 
+    ENGAGEMENT_POLL_CHATTING_APPS = _open_scheme("engagement_poll_chatting_apps.json")
+    ENGAGEMENT_POLL_ACCESS_TO_INTERNET = _open_scheme("engagement_poll_access_to_internet.json")
+    ENGAGEMENT_POLL_FACEBOOK_USAGE = _open_scheme("engagement_poll_facebook_usage.json")
+    ENGAGEMENT_POLL_RADIO_STATIONS = _open_scheme("engagement_poll_radio_stations.json")
+    ENGAGEMENT_POLL_OTHER_MESSAGES = _open_scheme("engagement_poll_other_messages.json")
+
     GENDER = _open_scheme("gender.json")
     AGE = _open_scheme("age.json")
     AGE_CATEGORY = _open_scheme("age_category.json")
